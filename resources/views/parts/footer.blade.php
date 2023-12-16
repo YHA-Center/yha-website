c521ba043d868e5eb901bb560b17c4e482757368
Hein-HtetSan <heinhtetsan33455@gmail.com>
2023-12-16 12:33:03 +0630

    update some function on <footer and navbar>

---
+++ resources/views/parts/footer.blade.php
@@ -0,0 +1,49 @@
+<div style="overflow: hidden; margin-top: 150px;">
+    <svg preserveAspectRatio="none" viewBox="0 0 1200 120" xmlns="http://www.w3.org/2000/svg"
+        style="fill: rgb(49, 52, 61); width: 100%; height: 100px; transform: rotate(180deg);">
+        <path
+            d="M321.39 56.44c58-10.79 114.16-30.13 172-41.86 82.39-16.72 168.19-17.73 250.45-.39C823.78 31 906.67 72 985.66 92.83c70.05 18.48 146.53 26.09 214.34 3V0H0v27.35a600.21 600.21 0 00321.39 29.09z" />
+    </svg>
+</div>
+<section id="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 pt-4">
+                <ul>
+                    <li><a class="active" href="index.php">Home</a></li>
+                    <li><a href="index.php">About</a></li>
+                    <li><a href="courses.php">Courses</a></li>
+                    <li><a href="index.php">Contact Us</a></li>
+                </ul>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12 pt-4">
+                <ul>
+                    <li><a href="stu_projects.php">Students Projects</a></li>
+                    <li><a href="event.php">Events</a></li>
+                    <li><a href="">Policy</a></li>
+                    <li><a href="">Rules</a></li>
+                    <li><a href="">Purpose</a></li>
+                </ul>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 col-12">
+                <div class="img">
+                    <img src="img/logo.png" alt="">
+                    <span>Build Your Future With Technology</span>
+                </div>
+            </div>
+            <div class="row copyright">
+                <div class="col-6 left">
+                    <span>Copyright @2023 YHA</span>
+                </div>
+                <div class="col-6 right">
+                    <span>Follow us on social media</span>
+                    <div class="icons">
+                        <a href="https://www.facebook.com/yhacomputerforyou"><i class="fa-brands fa-facebook"></i></a>
+                        <a href="https://www.facebook.com/messages/t/100787362478747"><i
+                                class="fa-brands fa-facebook-messenger"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
